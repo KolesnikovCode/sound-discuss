1d8ec83bf24d88834b874f3b2e3e261e1ce5b2bb
Yuri Kolesnikov <yk@goldcarrot.ru>
2020-01-18 20:56:32 +0300

    some

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
     return (
         <div className="container">
             <h1>Home Page</h1>
+            <div className="my-projects">
+                <h3>My projects</h3>
+                <div className="my-projects_wrapper">
+                    {
+                        [1,0,11,'youtube'].map((el) => (
+                            <div className="my-projects_project">
+                                <Link to="/project/project-id">{`Project ${el}`}</Link>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
